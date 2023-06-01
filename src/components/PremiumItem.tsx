ceb9407bc43a89757e2c347b0b94ed4258e678a0
Guilherme <guilherme.flesch92@gmail.com>
2023-06-01 17:52:17 -0300

    peso da fonte

--- src/components/PremiumItem.tsx
+++ src/components/PremiumItem.tsx
@@ -7,7 +7,7 @@ export default function PremiumItem(props: PremiumItemProps){
     return (
         <li className={`
             text-base 
-            tracking-wider font-bold
+            tracking-wider font-semibold
             hover:scale-105
             cursor-pointer
             ${props.className ?? ''}`}>{props.children}
